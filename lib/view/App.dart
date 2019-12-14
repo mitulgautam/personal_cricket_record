66f8caddff9c2f70e80170eeba5fc1728887b4b0
Mitul Gautam <mitulgautam@outlook.com>
2019-12-14 11:33:46 +0530

    init state
    imported
    
    firebase auth
    google apis
    provider
    google fonts
    getit injector

---
+++ lib/view/App.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class App extends StatefulWidget {
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("Hello"),
+      ),
+    );
+  }
+}
